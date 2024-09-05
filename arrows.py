5e71af14f2f83f858dcaa3e14a38bf737c6a0939
eg24sp <eg24sp@gmail.com>
2024-09-05 19:14:34 -0400

    Update arrows.py
    Made arrow collision more efficient.

--- arrows.py
+++ arrows.py
@@ -19,15 +19,13 @@ time_per_beat = 60 / BPM
 frames_per_beat = 30 * time_per_beat
 speed_pixels_per_frame = distance_to_target / frames_per_beat
 
-'''
-# Function to render and display text
-def render_text(screen, text, position, duration=0.5, color=(255, 255, 255), font_size=36):
-    font = pygame.font.SysFont('Arial', font_size)
-    text_surface = font.render(text, True, color)
-    screen.blit(text_surface, position)
-    pygame.display.flip()
-    time.sleep(duration)  # Display text for the specified duration
-    '''
+# Dictionary for direction to x coordinates
+xdict = {'left':200, 'down':500, 'up':400, 'right':700, 'left_down':100, 'right_down':800, 'left_up':300, 'right_up':600,
+         'left2':1120, 'down2':1420, 'up2':1320, 'right2':1620, 'left_down2':1020, 'right_down2':1720, 'left_up2':1220, 'right_up2':1520}
+# Dictionary for direction to button index
+p1dict = {'left':0, 'down':1, 'up':2, 'right':3, 'left_down':4, 'right_down':5, 'left_up':6, 'right_up':7}
+p2dict = {'left2':0, 'down2':1, 'up2':2, 'right2':3, 'left_down2':4, 'right_down2':5, 'left_up2':6, 'right_up2':7}
+
 
 class Arrow(pygame.sprite.Sprite):
     def __init__(self, image, position, direction):
@@ -59,38 +57,7 @@ class ArrowManager:
 
     def spawn_arrow(self, direction, x, y):
        # Determine position based on direction
-        if direction == "left_down":
-            x = 100
-        elif direction == "left":
-            x = 200
-        elif direction == "left_up":
-            x = 300
-        elif direction == "up":
-            x = 400
-        elif direction == "down":
-            x = 500
-        elif direction == "right_up":
-            x = 600
-        elif direction == "right":
-            x = 700
-        elif direction == "right_down":
-            x = 800
-        elif direction == "left_down2":
-            x = 1020
-        elif direction == "left2":
-            x = 1120
-        elif direction == "left_up2":
-            x = 1220
-        elif direction == "up2":
-            x = 1320
-        elif direction == "down2":
-            x = 1420
-        elif direction == "right_up2":
-            x = 1520
-        elif direction == "right2":
-            x = 1620
-        elif direction == "right_down2":
-            x = 1720
+        x = xdict[direction]
         self.add(x, y, direction)
 
     def update(self):
@@ -99,109 +66,22 @@ class ArrowManager:
     def draw(self):
         self.arrows.draw(self.screen)
 
-
     def check_collision(self, pygame, joy1, joy2, txtRndr):
         for arrow in self.arrows:
             if abs(arrow.rect.y - 75) < 75:
                 # Collision detected - check if the correct key is pressed
-                 
-                joystick1_pressed = None
-                if joy1:
-                    if joy1.get_button(0):  
-                        joystick1_pressed = "left"
-                    elif joy1.get_button(1):  
-                        joystick1_pressed = "down"
-                    elif joy1.get_button(2): 
-                        joystick1_pressed = "up"
-                    elif joy1.get_button(3): 
-                        joystick1_pressed = "right"
-                    elif joy1.get_button(4): 
-                        joystick1_pressed = "left_down"
-                    elif joy1.get_button(5): 
-                        joystick1_pressed = "right_down"
-                    elif joy1.get_button(6): 
-                        joystic1_pressed = "left_up"
-                    elif joy1.get_button(7): 
-                        joystick1_pressed = "right_up"
-                    elif joy1.get_button(8): 
-                        joystick1_pressed = "select"
-                    elif joy1.get_button(9): 
-                        joystick1_pressed = "back"
-
-                joystick2_pressed = None
-                if joy2:
-                    if joy2.get_button(0):  
-                        joystick2_pressed = "left2"
-                    elif joy2.get_button(1):  
-                        joystick2_pressed = "down2"
-                    elif joy2.get_button(2): 
-                        joystick2_pressed = "up2"
-                    elif joy2.get_button(3): 
-                        joystick2_pressed = "right2"
-                    elif joy2.get_button(4): 
-                        joystick2_pressed = "left_down2"
-                    elif joy2.get_button(5): 
-                        joystick2_pressed = "right_down2"
-                    elif joy2.get_button(6): 
-                        joystic1_pressed = "left_up2"
-                    elif joy2.get_button(7): 
-                        joystick2_pressed = "right_up2"
-                    elif joy2.get_button(8): 
-                        joystick2_pressed = "select2"
-                    elif joy2.get_button(9): 
-                        joystick2_pressed = "back2"
-                        
-                 
-                 # Check for collision with the correct input for player 1
-                if arrow.direction == "left" and joystick1_pressed == "left":
-                    txtRndr.show_text("HIT!", (200, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "down" and joystick1_pressed == "down":
-                    txtRndr.show_text("HIT!", (500, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "up" and joystick1_pressed == "up":
-                    txtRndr.show_text("HIT!", (400, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "right" and joystick1_pressed == "right":
-                    txtRndr.show_text("HIT!", (700, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "left_down" and joystick1_pressed == "left_down":
-                    txtRndr.show_text("HIT!", (100, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "right_down" and joystick1_pressed == "right_down":
-                    txtRndr.show_text("HIT!", (800, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "left_up" and joystick1_pressed == "left_up":
-                    txtRndr.show_text("HIT!", (300, 75))
-                elif arrow.direction == "right_up" and joystick1_pressed == "right_up":
-                    txtRndr.show_text("HIT!", (600, 75))
-                    self.arrows.remove(arrow)
-
-                # Check for collision with the correct input for player 2
-                if arrow.direction == "left2" and joystick2_pressed == "left2":
-                    txtRndr.show_text("HIT!", (1120, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "down2" and joystick2_pressed == "down2":
-                    txtRndr.show_text("HIT!", (1420, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "up2" and joystick2_pressed == "up2":
-                    txtRndr.show_text("HIT!", (1320, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "right2" and joystick2_pressed == "right2":
-                    txtRndr.show_text("HIT!", (1620, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "left_down2" and joystick2_pressed == "left_down2":
-                    txtRndr.show_text("HIT!", (1020, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "right_down2" and joystick2_pressed == "right_down2":
-                    txtRndr.show_text("HIT!", (1720, 75))
-                    self.arrows.remove(arrow)
-                elif arrow.direction == "left_up2" and joystick2_pressed == "left_up2":
-                    txtRndr.show_text("HIT!", (1220, 75))
-                elif arrow.direction == "right_up2" and joystick2_pressed == "right_up2":
-                    txtRndr.show_text("HIT!", (1520, 75))
-                    self.arrows.remove(arrow)      
-
+                index = None
+                # checking if arrow is on p2's side
+                if '2' in arrow.direction:
+                    index = p2dict[arrow.direction]
+                    if joy2.get_button(index):
+                        txtRndr.show_text("HIT!", (xdict[arrow.direction], 75))
+                        self.arrows.remove(arrow)
+                else:
+                    index = p1dict[arrow.direction]
+                    if joy1.get_button(index):
+                        txtRndr.show_text("HIT!", (xdict[arrow.direction], 75))
+                        self.arrows.remove(arrow)
             elif arrow.rect.y < 100:
                 arrow.miss(txtRndr) # generates miss text
                 self.arrows.remove(arrow)
