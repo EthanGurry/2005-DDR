5742d92d911772abf888f329cc6cebf34dafeff9
eg24sp <eg24sp@gmail.com>
2024-09-08 18:58:49 -0400

    Update midiConverter.py
    Fixed temp/bpm issue

--- midiConverter.py
+++ midiConverter.py
@@ -1,11 +1,15 @@
 import mido
 
+# dictionary for notes to direction
+dictNotes = {48: 'down', 49:'left_down', 50:'left', 52:'left_up', 53:'up', 55:'right_up', 57:'right', 59:'right_down',
+             60: 'down2', 61:'left_down2', 62:'left2', 64:'left_up2', 65:'up2', 67:'right_up2', 69:'right2', 71:'right_down2'}
+
 # Load the MIDI file
-midi_file = mido.MidiFile('midi_test_02_fixed.mid')
+midi_file = mido.MidiFile('song_test_V001.mid')
 
 # Extract relevant information: (note, velocity, time in ticks)
 notes = []
-tempo = 500000  # Default tempo (120 BPM) if not set by a meta message
+tempo = 521739.13  # Default tempo (120 BPM) if not set by a meta message
 ticks_per_beat = midi_file.ticks_per_beat  # PPQ (Pulses Per Quarter note)
 
 for track in midi_file.tracks:
@@ -17,7 +21,6 @@ for track in midi_file.tracks:
         if msg.type == 'note_on' and msg.velocity > 0:
             notes.append((msg.note, msg.velocity, absolute_time))
 
-
 # Convert ticks to time in seconds
 def ticks_to_seconds(ticks, tempo, ticks_per_beat):
     return (ticks / ticks_per_beat) * (tempo / 1000000)
@@ -25,38 +28,7 @@ def ticks_to_seconds(ticks, tempo, ticks_per_beat):
 # convert note to direction, p1 has lower octave
 def convert_notes(note):
     arrow = ''
-    if note == 49:
-        arrow = 'left_down'
-    elif note == 61:
-        arrow = 'left_down2'
-    elif note == 50:
-        arrow = 'left'
-    elif note == 62:
-        arrow = 'left2'
-    elif note == 52:
-        arrow = 'left_up'
-    elif note == 64:
-        arrow = 'left_up2'
-    elif note == 53:
-        arrow = 'up'
-    elif note == 65:
-        arrow = 'up2'
-    elif note == 55:
-        arrow = 'right_up'
-    elif note == 67:
-        arrow = 'right_up2'
-    elif note == 57:
-        arrow = 'right'
-    elif note == 69:
-        arrow = 'right2'
-    elif note == 59:
-        arrow = 'right_down'
-    elif note == 71:
-        arrow = 'right_down2'
-    if note == 60:
-        arrow = 'down'
-    elif note == 72:
-        arrow = 'down2'
+    arrow = dictNotes[note]
     return arrow
 
 # Convert all note events to real-time values
